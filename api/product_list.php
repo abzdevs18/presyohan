786b27684e4c7ced701c18b42b103988c26d4d8d
Clint.Abz <abz.devs@gmail.com>
2019-12-14 17:46:22 +0800

    Query TesT

--- api/product_list.php
+++ api/product_list.php
@@ -4,7 +4,7 @@ include '../config.php';
 $list = array();
 $query = mysqli_query($con,"SELECT * FROM products");
 while ($row = mysqli_fetch_assoc($query)) {
-	array_push($list, array("prod_id"=>$row['prod_id'],"prod_name"=>$row['prod_name'],"prod_price"=>$row['prod_price'],"prod_category"=>$row['prod_category'],"prod_img"=>$row['prod_img'],"description"=>$row['description']));
+	array_push($list, array("prod_id"=>$row['prod_id'],"prod_name"=>$row['prod_name'],"prod_price"=>number_format($row['prod_price']),"prod_category"=>$row['prod_category'],"prod_img"=>$row['prod_img'],"description"=>$row['description']));
 	// echo "<br />". $row['prod_name'] . "<br />";
 }
 
