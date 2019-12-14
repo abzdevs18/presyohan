4cb39d1e1b259ca6a806f3f3bb014ed208226162
Clint.Abz <abz.devs@gmail.com>
2019-12-14 17:38:04 +0800

    Query Tes

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
-$db_username = 'root';
-$db_password = 'D3b1an!?';
-$db_name = 'presyohan';
-$db_host = 'localhost';
-$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+// $db_username = 'root';
+// $db_password = 'D3b1an!?';
+// $db_name = 'presyohan';
+// $db_host = 'localhost';
+// $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 
-$con=mysqli_connect("localhost","root","D3b1an!?","presyohan");
+$con=new mysqli("localhost","root","D3b1an!?","presyohan");
 // Check connection
-if (mysqli_connect_error())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+// if (mysqli_connect_error())
+//   {
+//   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+//   }
 
 ?>
