47498752b4ebb6f16963c288025a6cf4f42b3197
Clint.Abz <abz.devs@gmail.com>
2019-12-14 16:45:51 +0800

    Query Tes

---
+++ api/cat_list.php
@@ -0,0 +1,13 @@
+<?php
+include '../config.php';
+
+$list = array();
+$query = mysqli_query($con,"SELECT DISTINCT(prod_category) AS cat FROM `products`");
+while ($row = mysqli_fetch_assoc($query)) {
+	$q = mysqli_query($con,"SELECT DISTINCT COUNT(prod_id) AS num,prod_name, description FROM `products` WHERE prod_category LIKE '". $row['cat']."' ");
+	$res = mysqli_fetch_assoc($q);
+	array_push($list, array("prod_category"=>$row['cat'],"description"=>$res['description'],"items"=>$res['num']));
+	// echo "<br />". $row['prod_name'] . "<br />";
+}
+
+echo json_encode($list);
---
+++ api/product_list.php
@@ -0,0 +1,11 @@
+<?php
+include '../config.php';
+
+$list = array();
+$query = mysqli_query($con,"SELECT * FROM products");
+while ($row = mysqli_fetch_assoc($query)) {
+	array_push($list, array("prod_id"=>$row['prod_id'],"prod_name"=>$row['prod_name'],"prod_price"=>$row['prod_price'],"prod_category"=>$row['prod_category'],"prod_img"=>$row['prod_img'],"description"=>$row['description']));
+	// echo "<br />". $row['prod_name'] . "<br />";
+}
+
+echo json_encode($list);
