020b687f17d9b0dc733a8b805519b6bf9a015446
Clint.Abz <abz.devs@gmail.com>
2019-12-17 13:39:19 +0800

    Data

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 // $db_name = 'presyohan';
 // $db_host = 'localhost';
 // $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+//mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $con=new mysqli("localhost","root","D3b1an!?","presyohan");
 // Check connection
 // if (mysqli_connect_error())
