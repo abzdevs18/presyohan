7191ca99c87b9fc799c5e21f2ec241122671fa59
Clint.Abz <abz.devs@gmail.com>
2019-12-17 11:49:50 +0800

    Check

--- api/cat_list.php
+++ api/cat_list.php
@@ -4,7 +4,7 @@ include '../config.php';
 $list = array();
 $query = mysqli_query($con,"SELECT DISTINCT(prod_category) AS cat FROM `products`");
 while ($row = mysqli_fetch_assoc($query)) {
-	$q = mysqli_query($con,"SELECT DISTINCT COUNT(prod_id) AS num,prod_name, description FROM `products` WHERE prod_category LIKE 'Mens Shoes'");
+	$q = mysqli_query($con,"SELECT DISTINCT COUNT(prod_id) AS `num`,`prod_name`, `description` FROM `products` WHERE `prod_category` LIKE 'Mens Shoes'");
 	$res = mysqli_fetch_assoc($q);
 	array_push($list, array("prod_category"=>$row['cat'],"description"=>$res['description'],"items"=>$res['num']));
 	// echo "<br />". $row['prod_name'] . "<br />";
